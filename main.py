a44e80eb7f745720c71589aa4418ffcc26329a4e
Alexandre Cunha <casadacrianca11@gmail.com>
2021-05-04 12:43:39 -0300

    menu finalizado e rotas criadas principal

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ app = Flask(__name__)
 
 
 @app.route('/home')
-def home():
-    return render_template('home.html')
+def dashboard():
+    return render_template('dashboard.html')
 
+@app.route('/mapa')
+def mapa():
+    return render_template('mapa_interativo.html')
+
+@app.route('/info')
+def artigo():
+    return render_template('artigos_interativos.html')
+
+@app.route('/export')
+def info():
+    return render_template('infografico_interativo.html')
 
 
 if __name__=='__main__':
