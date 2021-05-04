883290e58b24f54f70b98fbb6a9c453b5fdd2eb6
Alexandre Cunha <casadacrianca11@gmail.com>
2021-05-04 12:35:33 -0300

    menu finalizado e rotas criadas

--- teste/main.py
+++ teste/main.py
@@ -8,6 +8,22 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/grid')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/mapa')
+def mapa():
+    return render_template('mapa_interativo.html')
+
+@app.route('/info')
+def artigo():
+    return render_template('artigos_interativos.html')
+
+@app.route('/export')
+def info():
+    return render_template('infografico_interativo.html')
+
 
 if __name__=='__main__':
     app.run(debug=True)
