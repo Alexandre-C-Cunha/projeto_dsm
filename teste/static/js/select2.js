991152b308548992cf90bf754d136950ba45c2f4
Alexandre Cunha <casadacrianca11@gmail.com>
2021-06-08 10:35:10 -0300

    Listae escolha select2

--- teste/static/js/select2.js
+++ teste/static/js/select2.js
@@ -1,4 +1,38 @@
 // In your Javascript (external .js resource or <script> tag)
 $(document).ready(function() {
-    $('.js-example-basic-multiple').select2();
+  $('.js-data-example-ajax').select2({
+  minimumInputLength: 2,
+  placeholder: 'Search',
+  ajax: {
+    url: "/rota_jason",
+    dataType: 'json',
+    delay: 250,
+        
+    // Additional AJAX parameters go here; see the end of this chapter for the full code of this example
+  }
 });
+  $('.js-data-example-ajax').on('select2:select', function (e) {
+    var data = e.params.data;
+    value= $('.js-data-example-ajax').select2('data');
+    var value3=''
+    for(var i=0 in value) {
+      value3+=value[i].text+',';
+      }
+    fetch('/rota_escolha/' + value3).then(function(response) {
+      
+        });
+});
+
+  $('.js-data-example-ajax').on('select2:unselect', function (e) {
+    var data = e.params.data;
+    value= $('.js-data-example-ajax').select2('data');
+    var value3=''
+    for(var i=0 in value) {
+      value3+=value[i].text+',';
+      }
+    fetch('/rota_escolha/' + value3).then(function(response) {
+      
+        });
+});
+});
+
--- teste/teste_lista_busca.py
+++ teste/teste_lista_busca.py
@@ -1,17 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
+import os
 from guppy import hpy
 from memory_profiler import profile
+from flask import jsonify, json, current_app as app
+import wtforms as wt
+from wtforms import TextField, Form
+
 
 app = Flask(__name__)
 
-df_lista_de_Busca_final=pd.read_pickle('dados_tabela/df_lista_busca.pickle')
+def acentuacao(x):
+    from unicodedata import normalize
+    source = x
+    target = normalize('NFKD', source).encode('ASCII','ignore').decode('ASCII')
+    return(target)
 
-@app.route('/')
+
+df_lista=pd.read_json('static\\data\\api.jason')
+df_lista['NOME']=df_lista['NOME'].apply(lambda x: acentuacao(x).upper())
+df_lista=df_lista.drop_duplicates()
+
+@app.route('/rota_jason')
+def rota_jason():
+	termo = request.args.get('term')
+	a=acentuacao(termo).upper()
+	df_term=df_lista[df_lista['NOME'].str.contains(a)]
+	lista_amarradinho=[]
+	for index, row in df_term.iterrows():
+		lista_amarradinho.append({'id':index,'text':row['NOME']+" ("+row['TIPO']+")"})
+	final={'results':lista_amarradinho}
+	with open('static\\data\\lista_amarradinho.json', 'w') as outfile:
+		json.dump(final, outfile)
+	teste_arq= open('static\\data\\lista_amarradinho.json')
+	data = json.load(teste_arq)
+	return (data)
+
+@app.route('/rota_escolha/<todas>')
+def rota_escolha(todas):
+	Sfiltro2 = str(todas)
+	print(Sfiltro2)
+	return jsonify({'Sfiltro2':Sfiltro2})
+
+@app.route('/home/')
 def home():
-	df=df_lista_de_Busca_final.sample(2000)
-	return render_template('select2.html',df=df)
-
+	
+	return render_template("select2.html")
 
 
 if __name__=='__main__':
